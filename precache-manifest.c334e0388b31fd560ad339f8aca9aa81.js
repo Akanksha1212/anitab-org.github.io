aba52eb0ebf5ec98e2f81477af65d2d42027ea17
Akanksha Singh <akankshasingh121297@gmail.com>
2020-09-11 08:04:29 +0530

    Updates

---
+++ precache-manifest.c334e0388b31fd560ad339f8aca9aa81.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deebd56b1e1d3ce3923338b829a895c0",
+    "url": "/anitab-org.github.io/index.html"
+  },
+  {
+    "revision": "a6222f136475ac3da608",
+    "url": "/anitab-org.github.io/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "59453a10414b0087538e",
+    "url": "/anitab-org.github.io/static/js/2.8a3722d2.chunk.js"
+  },
+  {
+    "revision": "c2496f987363ee1a26277a53c25e3675",
+    "url": "/anitab-org.github.io/static/js/2.8a3722d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6222f136475ac3da608",
+    "url": "/anitab-org.github.io/static/js/main.0e250a06.chunk.js"
+  },
+  {
+    "revision": "e6d437a3d13779e59a72",
+    "url": "/anitab-org.github.io/static/js/runtime-main.35556402.js"
+  },
+  {
+    "revision": "552c358973068e62c7a41bbbcce2fc67",
+    "url": "/anitab-org.github.io/static/media/contribute.552c3589.png"
+  },
+  {
+    "revision": "5a653ec391ddcff0753c57d08a3c2eb9",
+    "url": "/anitab-org.github.io/static/media/linkedin-logo.5a653ec3.png"
+  },
+  {
+    "revision": "45cc4ce0406ddd458e03a61f586ea483",
+    "url": "/anitab-org.github.io/static/media/logo.45cc4ce0.png"
+  },
+  {
+    "revision": "b19b61ad9e12c23fe0e4f6c34b9d1f69",
+    "url": "/anitab-org.github.io/static/media/our-projects.b19b61ad.png"
+  },
+  {
+    "revision": "66c6a83d8e650fe44c591ef600271c13",
+    "url": "/anitab-org.github.io/static/media/placeholder.66c6a83d.png"
+  }
+]);
